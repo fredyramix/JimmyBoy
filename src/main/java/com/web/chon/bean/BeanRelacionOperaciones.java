9cb0d35349ca5cadcf55987f8f7d0428e516f782
fredfyramix <freddyramix@gmail.com>
2016-08-14 23:30:00 -0500

    Agregue impresiones en relación de operaciones

--- src/main/java/com/web/chon/bean/BeanRelacionOperaciones.java
+++ src/main/java/com/web/chon/bean/BeanRelacionOperaciones.java
@@ -1,17 +1,47 @@
 package com.web.chon.bean;
+
 import com.web.chon.dominio.Usuario;
+import com.web.chon.dominio.Venta;
 import com.web.chon.dominio.Ventas;
 import com.web.chon.dominio.VentasProductos;
 import com.web.chon.service.IfaceCatUsuario;
 import com.web.chon.service.IfaceVentas;
+import com.web.chon.util.Constantes;
+import com.web.chon.util.JasperReportUtil;
 import com.web.chon.util.JsfUtil;
+import com.web.chon.util.NumeroALetra;
 import com.web.chon.util.TiempoUtil;
+import com.web.chon.util.UtilUpload;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.StreamedContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -21,8 +51,10 @@ import org.springframework.stereotype.Component;
 public class BeanRelacionOperaciones implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Autowired private IfaceVentas ifaceVentas;
-    @Autowired private IfaceCatUsuario ifaceCatUsuario;
+    @Autowired
+    private IfaceVentas ifaceVentas;
+    @Autowired
+    private IfaceCatUsuario ifaceCatUsuario;
     private ArrayList<Ventas> listaVentasHistorial;
     private ArrayList<Usuario> listaMeseros;
     private String title = "";
@@ -40,10 +72,19 @@ public class BeanRelacionOperaciones implements Serializable {
     private boolean enableCalendar;
     private BigDecimal totalRelacion;
     private BigDecimal idMeseroFk;
+    private StreamedContent media;
+
+    //variables para ticket
+    private Map paramReport = new HashMap();
+    private ByteArrayOutputStream outputStream;
+    private String number;
+    private String rutaPDF;
+    private String nombreEmpaque = "";
+    private String pathFileJasper = "";
+    private Ventas ventaImprimir;
 
     @PostConstruct
-    public void init() 
-    {
+    public void init() {
         listaMeseros = new ArrayList<Usuario>();
         status = 2;
         setTitle("Relación de Operaciones");
@@ -53,6 +94,88 @@ public class BeanRelacionOperaciones implements Serializable {
         listaVentasHistorial = new ArrayList<Ventas>();
         fechaFiltroInicio = new Date();
         fechaFiltroFin = new Date();
+
+    }
+
+    public void imprimir() {
+        setParameterTicket(ventaImprimir);
+        generateReport(ventaImprimir.getIdVentaPk().intValue(), ventaImprimir.getFolio().intValue());
+        RequestContext.getCurrentInstance().execute("window.frames.miFrame.print();");
+    }
+
+    private void setParameterTicket(Ventas venta) {
+
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.getDefault());
+        DecimalFormat df = new DecimalFormat("###.##");
+        Date date = new Date();
+        NumeroALetra numeroLetra = new NumeroALetra();
+        String totalVentaStr = numeroLetra.Convertir(df.format(venta.getTotal()), true);
+        putValues(TiempoUtil.getFechaDDMMYYYYHHMM(date), totalVentaStr, venta, nf.format(venta.getTotal()));
+
+    }
+    private void putValues(String dateTime, String totalVentaStr, Ventas v, String totalisimo) {
+        paramReport.put("fechaVenta", TiempoUtil.getFechaDDMMYYYYHHMM(v.getFechaFin()));
+        paramReport.put("folio", v.getFolio().toString());
+        paramReport.put("ID_VENTA_PK", v.getIdVentaPk().toString());
+        paramReport.put("ventaTotal", totalisimo);
+        paramReport.put("totalLetra", totalVentaStr);
+        paramReport.put("mesa", v.getNumeroMesa().toString());
+        paramReport.put("mesero", v.getNombreMesero());
+        paramReport.put("per", v.getCantidadPersonas().toString());
+        paramReport.put("abre", v.getFechaInicio().getHours() + ":" + v.getFechaInicio().getMinutes() + ":" + v.getFechaInicio().getSeconds() + "hrs");
+        paramReport.put("cie", v.getFechaFin().getHours() + ":" + v.getFechaFin().getMinutes() + ":" +v.getFechaFin().getSeconds() + "hrs");
+    }
+
+    public void generateReport(int idVenta, int folioVenta) {
+        JRExporter exporter = null;
+
+        try {
+            ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+            String temporal = "";
+            if (servletContext.getRealPath("") == null) {
+                temporal = Constantes.PATHSERVER;
+            } else {
+                temporal = servletContext.getRealPath("");
+            }
+            Context initContext;
+            Connection con = null;
+            try {
+
+                javax.sql.DataSource datasource = null;
+
+                Context initialContext = new InitialContext();
+
+                // "jdbc/MyDBname" >> is a JNDI Name of DataSource on weblogic
+                datasource = (DataSource) initialContext.lookup("DataChon");
+
+                try {
+                    con = datasource.getConnection();
+                    System.out.println("datsource" + con.toString());
+                } catch (SQLException ex) {
+                    Logger.getLogger(beanCorteCaja.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (NamingException ex) {
+                Logger.getLogger(beanCorteCaja.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            pathFileJasper = temporal + File.separatorChar + "resources" + File.separatorChar + "report" + File.separatorChar + "ticketVenta" + File.separatorChar + "ticketVentaV2.jasper";
+
+            JasperPrint jp = JasperFillManager.fillReport(getPathFileJasper(), paramReport, con);
+            outputStream = JasperReportUtil.getOutputStreamFromReport(paramReport, getPathFileJasper());
+            exporter = new JRPdfExporter();
+
+            exporter.setParameter(JRExporterParameter.JASPER_PRINT, jp);
+            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outputStream);
+//            exporter.setParameter(JRPdfExporterParameter.PDF_JAVASCRIPT, "this.print();");
+            byte[] bytes = outputStream.toByteArray();
+
+            rutaPDF = UtilUpload.saveFileTemp(bytes, "ticketPdf", idVenta, 1);
+            con.close();
+
+        } catch (Exception exception) {
+            System.out.println("Error >" + exception.getMessage());
+
+        }
 
     }
 
@@ -218,6 +341,70 @@ public class BeanRelacionOperaciones implements Serializable {
 
     public void setHoy(Date hoy) {
         this.hoy = hoy;
+    }
+
+    public Ventas getVentaImprimir() {
+        return ventaImprimir;
+    }
+
+    public void setVentaImprimir(Ventas ventaImprimir) {
+        this.ventaImprimir = ventaImprimir;
+    }
+
+    public StreamedContent getMedia() {
+        return media;
+    }
+
+    public void setMedia(StreamedContent media) {
+        this.media = media;
+    }
+
+    public Map getParamReport() {
+        return paramReport;
+    }
+
+    public void setParamReport(Map paramReport) {
+        this.paramReport = paramReport;
+    }
+
+    public ByteArrayOutputStream getOutputStream() {
+        return outputStream;
+    }
+
+    public void setOutputStream(ByteArrayOutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getRutaPDF() {
+        return rutaPDF;
+    }
+
+    public void setRutaPDF(String rutaPDF) {
+        this.rutaPDF = rutaPDF;
+    }
+
+    public String getNombreEmpaque() {
+        return nombreEmpaque;
+    }
+
+    public void setNombreEmpaque(String nombreEmpaque) {
+        this.nombreEmpaque = nombreEmpaque;
+    }
+
+    public String getPathFileJasper() {
+        return pathFileJasper;
+    }
+
+    public void setPathFileJasper(String pathFileJasper) {
+        this.pathFileJasper = pathFileJasper;
     }
 
 }
